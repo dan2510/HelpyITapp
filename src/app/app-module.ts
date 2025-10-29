70f88f82621f2bf2d9f8ca4b9038d4398e8e0877
dan2510 <carballo2597@gmail.com>
2025-10-29 04:48:04 -0600

    toda la configuracion de angular y

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { CoreModule } from './core/core-module';
+import { ShareModule } from './share/share-module';
+import { HomeModule } from './home/home-module';
+import { UsuariosModule } from './usuarios/usuarios-module';
+import { TiqueteModule } from './tiquete/tiquete-module';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgxSonnerToaster } from 'ngx-sonner'
+import { HttpErrorInterceptorService } from './share/interceptor/http-error-interceptor.service';
+
 
 @NgModule({
   declarations: [
@@ -10,10 +19,22 @@ import { App } from './app';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule,
+    ShareModule,
+    HomeModule,
+    UsuariosModule,
+    TiqueteModule,
+    NgxSonnerToaster,
+     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+      provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS, 
+      useClass: HttpErrorInterceptorService, 
+      multi:true
+    }
   ],
   bootstrap: [App]
 })
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+apiURL:"http://localhost:3000",
+endPointTiquete: 'tiquete',
+endPointGenero: 'genero',
+endPointPlataforma: 'plataforma',
+endPointOrden:'orden',
+endPointUser:'usuario',
+endPointRol:'rol',
+endPointReporte:'reporte'
+};
